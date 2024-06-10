d8235222d418531a3adde4b0319bff3478d74164
Sylvia-iannarelli <sylvia_iannarelli@hotmail.com>
2024-06-10 17:07:34 +0200

    Mise en place Symfo + base entité Recipe, RecipeController et Template

---
+++ src/Controller/RecipeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\RecipeRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class RecipeController extends AbstractController
+{
+    #[Route('/', name: 'app_recipe_index', methods: "GET")]
+    public function index(RecipeRepository $recipeRepository): Response
+    {
+        return $this->render('recipe/index.html.twig', [
+            'recipes' => $recipeRepository->findAll(),
+        ]);
+    }
+}
---
+++ src/Entity/Recipe.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RecipeRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RecipeRepository::class)]
+class Recipe
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::JSON)]
+    private array $ingredients = [];
+
+    #[ORM\Column(length: 255)]
+    private ?string $preparationTime = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $cookingTime = null;
+
+    #[ORM\Column(type: Types::SMALLINT)]
+    private ?int $serves = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getIngredients(): array
+    {
+        return $this->ingredients;
+    }
+
+    public function setIngredients(array $ingredients): static
+    {
+        $this->ingredients = $ingredients;
+
+        return $this;
+    }
+
+    public function getPreparationTime(): ?string
+    {
+        return $this->preparationTime;
+    }
+
+    public function setPreparationTime(string $preparationTime): static
+    {
+        $this->preparationTime = $preparationTime;
+
+        return $this;
+    }
+
+    public function getCookingTime(): ?string
+    {
+        return $this->cookingTime;
+    }
+
+    public function setCookingTime(string $cookingTime): static
+    {
+        $this->cookingTime = $cookingTime;
+
+        return $this;
+    }
+
+    public function getServes(): ?int
+    {
+        return $this->serves;
+    }
+
+    public function setServes(int $serves): static
+    {
+        $this->serves = $serves;
+
+        return $this;
+    }
+}
