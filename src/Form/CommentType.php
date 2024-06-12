631176f96a10fd02009e82296abfda5fb946e195
Sylvia-iannarelli <sylvia_iannarelli@hotmail.com>
2024-06-12 11:35:46 +0200

    Ajustement formulaire d'ajout d'un commentaire

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Comment;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +14,25 @@ class CommentType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('content')
-            ->add('rating')
+            ->add('content', TextareaType::class, [
+                "label" => "Votre commentaire",
+                "attr" => [
+                    "placeholder" => "Donnez-nous votre avis sur cette recette ici..."
+                ]
+            ])
+            ->add('rating', ChoiceType::class, [
+                "label" => "Votre note*",
+                "choices" => [
+                    "Excellent" => 5,
+                    "Très bon" => 4,
+                    "Plutôt bon" => 3,
+                    "Bof" => 2,
+                    "Pas deux fois..." => 1
+                ],
+                "expanded" => true,
+                "multiple" => false,
+                "help" => "* une seule réponse possible"
+            ])
         ;
     }
 
